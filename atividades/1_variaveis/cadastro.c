ee8d20335c9e9cc7598c1aa8b94c41a42c9503d7
LUCAS FRANCISCO FERREIRA CUSTÓDIO <lucas.custodio@a.ucb.br>
2023-08-14 21:04:42 -0300

    feat: add 1_variaveis activity

---
+++ atividades/1_variaveis/cadastro.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    const int SIZE = 50;
+    char nome[SIZE];
+    int idade;
+    char matricula[SIZE];
+    char curso[SIZE];
+    int periodo;
+    char disciplinas[SIZE];
+    float valorMensalidade;
+
+    printf("Digite o nome: ");
+    fflush(stdin); // limpa o buffer do teclado
+    fgets(nome, SIZE, stdin);
+
+    printf("Digite a IDADE: ");
+    fflush(stdin); // limpa o buffer do teclado
+    scanf("%d", &idade);
+
+    printf("Digite a MATRICULA: ");
+    fflush(stdin); // limpa o buffer do teclado
+    fgets(matricula, SIZE, stdin);
+
+    printf("Digite o CURSO: ");
+    fflush(stdin); // limpa o buffer do teclado
+    fgets(curso, SIZE, stdin);
+
+    printf("Digite o PERIODO: ");
+    fflush(stdin); // limpa o buffer do teclado
+    scanf("%d", &periodo);
+
+    printf("Digite as DISCIPLINAS: ");
+    fflush(stdin); // limpa o buffer do teclado
+    fgets(disciplinas, SIZE, stdin);
+
+    printf("Digite o VALOR DA MENSALIDADE: ");
+    fflush(stdin); // limpa o buffer do teclado
+    scanf("%f", &valorMensalidade);
+
+    printf("\n- - - - -\n\n");
+
+    printf("NOME: %s", nome);
+    printf("IDADE: %d \n", idade);
+    printf("MATRICULA: %s", matricula);
+    printf("CURSO: %s", curso);
+    printf("PERIODO: %d \n", periodo);
+    printf("DISCIPLINAS: %s", disciplinas);
+    printf("VALOR DA MENSALIDADE: %f", valorMensalidade);
+
+    return 0;
+}
